1e41cce0664e36c772568b2dbf46a87bab59732b
hotdog2016 <yujinxing2013@126.com>
2020-04-09 14:41:05 +0800

    2020-04-09 14:41:05

---
+++ bishi/inc/list.h
@@ -0,0 +1,15 @@
+#ifndef LIST_H
+#define LIST_H
+
+struct list {
+    int num;
+    struct list* next;
+};
+
+struct list* creat_list(int* num , int length);
+int show_list(struct list** head);
+
+struct list* find_node(struct list** head, int num);
+int delete_node(struct list** head, struct list* node);
+
+#endif /* end of include guard: LIST_H */
